d65493df8dd387820d8f1997ad9a8678592d753b
luc laurent <laurentluc7@gmail.com>
2022-11-07 13:16:42 +0100

    remove setting on Navbar

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -24,7 +24,6 @@ const Navbar: React.FC<Props> = ({ children }) => {
   ]
   const userNavigation = [
     { name: 'Your Profile', href: '/profile' },
-    { name: 'Settings', href: '#' },
     // { name: 'Sign out', href: '#' },
   ]
 
@@ -63,7 +62,6 @@ const Navbar: React.FC<Props> = ({ children }) => {
           {({ open }) => (
             <>
               <div className="">
-                {/* <div className="mx-auto max-w-7xl px-4 py-6 lg:py-3 sm:px-6 lg:px-8"> */}
 
                 <div className="relative flex justify-between">
                   <div className="flex items-center justify-between w-full fixed px-8 py-4 z-40 shadow-md bg-white top-0 right-0 left-0">
@@ -78,27 +76,6 @@ const Navbar: React.FC<Props> = ({ children }) => {
                         </a>
                       </div>
                     </div>
-                    {/* <div className="min-w-0 flex-1 md:px-8 lg:px-0 xl:col-span-6">
-                    <div className="flex items-center px-6 py-4 md:mx-auto md:max-w-3xl lg:mx-0 lg:max-w-none xl:px-0">
-                      <div className="w-full">
-                        <label htmlFor="search" className="sr-only">
-                          Search
-                        </label>
-                        <div className="relative">
-                          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                            <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                          </div>
-                          <input
-                            id="search"
-                            name="search"
-                            className="block w-full rounded-md border border-gray-300 bg-white py-2 pl-10 pr-3 text-sm placeholder-gray-500 focus:border-indigo-500 focus:text-gray-900 focus:placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
-                            placeholder="Search"
-                            type="search"
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  </div> */}
                     <div className="flex items-center lg:hidden">
                       {/* Mobile menu button */}
                       <Popover.Button className="-mx-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500">
