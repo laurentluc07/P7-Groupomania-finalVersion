f9b8cac7c1e27529cdb12b6277f68b6500eb3797
luc laurent <laurentluc7@gmail.com>
2022-11-07 22:19:42 +0100

    remove space

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -3,16 +3,16 @@
 //============================================================================
 
 module.exports = {
-    HOST: 'localhost',
-    USER: 'root',
-    PASSWORD: 'LUC070698!',
-    DB: 'groupomania_db',
-    dialect: 'mysql',
+  HOST: 'localhost',
+  USER: 'root',
+  PASSWORD: 'LUC070698!',
+  DB: 'groupomania_db',
+  dialect: 'mysql',
 
-    pool: {
-        max: 5, //the maximum number of connections in the pool
-        min: 0, //the minimum number of connections in the pool
-        acquire: 30000, //La durée maximale, en millisecondes, pendant laquelle ce pool essaiera d'obtenir une connexion avant de générer une erreur
-        idle: 10000 //Durée maximale, en millisecondes, pendant laquelle une connexion peut être inactive avant d'être libérée.
-    }
+  pool: {
+    max: 5, //the maximum number of connections in the pool
+    min: 0, //the minimum number of connections in the pool
+    acquire: 30000, //La durée maximale, en millisecondes, pendant laquelle ce pool essaiera d'obtenir une connexion avant de générer une erreur
+    idle: 10000 //Durée maximale, en millisecondes, pendant laquelle une connexion peut être inactive avant d'être libérée.
+  }
 }
